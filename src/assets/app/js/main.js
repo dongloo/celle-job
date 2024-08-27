7ddc47e253df022848e318e2e97b021d841b9202
Dong Loo <minhkidong@gmail.com>
2024-08-27 13:11:22 +0700

    update

--- src/assets/app/js/main.js
+++ src/assets/app/js/main.js
@@ -20,21 +20,7 @@ menuCloseBtn.addEventListener('click', function() {
 });
 
 
-const heroSlider = new Swiper('.hero-slider', {
-    loop: true,
-    slidesPerView: 1,
-    grabCursor: true,
-    effect: "fade",
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".hero-swiper-pagination",
-        clickable: true,
-    },
 
-});
 const logoSlider = new Swiper('.logo-slider', {
     loop: true,
     slidesPerView: 1,
@@ -60,102 +46,14 @@ const logoSlider = new Swiper('.logo-slider', {
         },
         1200: {
             slidesPerView: 5,
+        },
+        1440: {
+            slidesPerView: 6,
+        },
+        1640: {
+            slidesPerView: 7,
         }
       }
 
 });
-const productSlider = new Swiper('.product-slider', {
-    slidesPerView: 1,
-    spaceBetween: 32,
-    grabCursor: true,
-    pagination: {
-        el: ".product-swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        640: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        1280: {
-            slidesPerView: 3,
-        }
-    }
 
-});
-
-// MENU SCROLL
-var fixPx = 5;
-var lastId;
-var topMenu = document.getElementById("header");
-var topMenuHeight = topMenu.offsetHeight + fixPx;
-
-// All list items
-var menuItems = topMenu.querySelectorAll(".nav-item");
-
-// Anchors corresponding to menu items
-var scrollItems = Array.from(menuItems).map(function(item) {
-    var href = item.getAttribute("href");
-    var element = document.querySelector(href);
-    return element ? element : null;
-}).filter(Boolean);
-
-menuItems.forEach(function(item) {
-    item.addEventListener("click", function(e) {
-        var href = this.getAttribute("href");
-        var id = href.replace('#', '');
-        document.querySelectorAll('.nav-item-target').forEach(function(el) {
-            el.classList.remove('active');
-        });
-        document.getElementById(id).classList.add('active');
-        var offsetTop = href === "#" ? 0 : document.querySelector(href).offsetTop - topMenuHeight + fixPx;
-        window.scrollTo({
-            top: offsetTop - fixPx,
-            behavior: 'smooth'
-        });
-        closeMenu();
-        e.preventDefault();
-    });
-});
-
-window.addEventListener("scroll", function() {
-    //var fromTop = window.pageYOffset + topMenuHeight;
-    var fromTop = window.scrollY + topMenuHeight;
-    var cur = scrollItems.filter(function(item) {
-        return item.offsetTop - fixPx < fromTop;
-    });
-    cur = cur[cur.length - 1];
-    var id = cur ? cur.id : "uber-uns";
-    if (lastId !== id) {
-        lastId = id;
-        menuItems.forEach(function(item) {
-            item.classList.remove("active");
-        });
-        var activeItem = topMenu.querySelectorAll("[href='#" + id + "']");
-        if (activeItem) {
-            activeItem.forEach(function(item) {
-                item.classList.add("active");
-            });
-        }
-    }
-});
-
-document.getElementById('chatbot-toggle').addEventListener('click', function() {
-    if (document.getElementById('chatbot-iframe-area').classList.contains('active')) {
-        document.getElementById('chatbot-iframe-area').classList.remove('active');
-    } else {
-        document.getElementById('chatbot-iframe-area').classList.add('active');
-    }
-    if (!document.getElementById('chatbot-iframe').getAttribute('src')) {
-        document.getElementById('chatbot-iframe').setAttribute('src', document.getElementById('chatbot-iframe').getAttribute('data-src'));
-    }
-
-});
-
-document.addEventListener('click', function(e) {
-    if (!document.getElementById('chatbot-toggle').contains(e.target) && !document.getElementById('chatbot-iframe-area').contains(e.target)) {
-        document.getElementById('chatbot-iframe-area').classList.remove('active');
-    }
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,33 +8,33 @@ module.exports = {
         'brockmann': ["Brockmann", "Segoe UI", "Arial", "Segoe", "DejaVu Sans", "Trebuchet MS", "Verdana", "sans-serif"],
       },
       colors: {
-        'blue': {
-          950: '#151458',
-          1000: '#111047',
+        'primary': {
+          100: '#f0f5ff',
+          120: '#ebf2ff',
+          140: '#e6efff',
+          160: '#e1ecff',
+          180: '#dce9ff',
+          200: '#d6e4ff',
+          300: '#adc6ff',
+          400: '#85a5ff',
+          500: '#597ef7',
+          600: '#2f54eb',
+          700: '#1d39c4',
+          800: '#10239e',
+          900: '#061178',
+          1000: '#030852',
         },
-        'purple': {
-          750: '#7822A7',
+        'sky': {
+          1000: '#003440',
         }
       }
     },
     container: {
       center: true,
       screens: {
-        DEFAULT: '100%',
-        sm: '100%',
-        md: '100%',
-        lg: '100%',
-        xl: '1280px',
-        '2xl': '1280px',
+        '2xl': '1320px',
       },
-      padding: {
-        DEFAULT: '1.5rem',
-        sm: '1.5rem',
-        md: '1.5rem',
-        lg: '1.5rem',
-        xl: '2rem',
-        '2xl': '2rem',
-      },
+      padding: '1.5rem',
  
     }
   },
